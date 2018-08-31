5cc716262c5765cc3782416331fece6902bcd5bf
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-31 23:56:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(range) { 
   return function(startB, endB) { 
-    let actualStartB = startB.parseInt();
+    let actualStartB = parseInt(startB);
     let actualRange = Math.abs(startB - endB);
     let diff = Math.abs(range - actualRange); 
     return actualStartB;
