1126850ed8fce4769b3d0f108f89e57755703607
Daniel Valmas <daniel.valmas@gmail.com>
2018-09-01 00:13:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function produceDrivingRange(range) { 
+  return function(startB, endB) { 
+    let actualStartB = parseInt(startB);
+    let actualEndB = parseInt(endB);
+    let actualRange = Math.abs(actualStartB - actualEndB);
+    let diff = Math.abs(range - actualRange); 
+    
+    
+    if (range > actualRange) { 
+    return `within range by ${diff}`; 
+  } else { 
+    return `${diff} blocks out of range`;
+  }
+  }
+} 
+
+function produceTipCalculator(percent) { 
+  return function(fare) { 
+    return fare*percent; 
+  } 
+} 
+
+ 
